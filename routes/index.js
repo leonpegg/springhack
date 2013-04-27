3967c0e587c742a8ec293d48fc5bdc451ab0e6c2
Rob Hinchliff <robbie_hinch@yahoo.com>
2013-04-27 17:18:38 +0100

    added caching for bike data so we don't hit TFL too often and can use cache when TFL returns empty string

--- routes/index.js
+++ routes/index.js
@@ -28,8 +28,20 @@ exports.policeData = function(req, res) {
 	});
 };
 
+var BIKE_UPDATE_INTERVAL = 1000 * 60 * 3;	//3 minutes in ms
 
 exports.bikes = function(req, res) {
+
+	var bikedata = {};
+	if (global.bikedata) {
+		bikedata = global.bikedata;
+		var time_diff = Date.now() - bikedata.timestamp;	//in milliseconds
+		if (BIKE_UPDATE_INTERVAL > time_diff) {
+			console.log('sending bike data from global cache');
+			res.send(bikedata.data);
+			return;
+		}
+	}
 
 	var body = '';
 	var getReq = http.get('http://www.tfl.gov.uk/tfl/syndication/feeds/cycle-hire/livecyclehireupdates.xml', function(data) {
@@ -37,12 +49,22 @@ exports.bikes = function(req, res) {
   			body += chunk;
   		});
   		data.on("end", function() {
-			// console.log('bikes_body', body);
+			console.log('Bike data request complete');
+			//sometimes tfl returns an empty string for the bike data
+			if (!body || '' == body && bikedata.data) {
+				console.log('Bike data returned empty string');
+				res.send(bikedata.data);
+				return;
+			}
 			parseString(body, function (err, result) {
+				console.log('succesfully parsed bike xml into json');
+				global.bikedata = {data:result, timestamp: Date.now()};
 				res.send(result);
 			});
   		});
 	}).on('error', function(e) {
-  		//console.log('ERROR: ' + e.message);
+  		console.log('ERROR downloading bike data: ' + e.message);
+		if (bikedata)
+			res.send(bikedata.data);	//send latest cache of bike data
 	});
 };
