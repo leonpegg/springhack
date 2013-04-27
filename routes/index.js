d586f55ff80029236576f4e88e0660b9f9152cd9
Jeconais <jeconais@gmail.com>
2013-04-27 17:27:25 +0100

    Police data change

--- routes/index.js
+++ routes/index.js
@@ -1,4 +1,5 @@
 var http = require("http");
+var url = require("url");
 /*
  * GET home page.
  */
@@ -7,21 +8,3 @@ exports.index = function(req, res){
   res.render('index');
 };
 
-exports.policeData = function(req, res) {
-	var options = {
-  		host: 'data.police.uk',
-  		path: '/api/leicestershire/C01/crime'
-	};
-
-	var body = '';
-	var getReq = http.get(options, function(data) {
-  		data.on("data", function(chunk) {
-  			body += chunk;
-  		});
-  		data.on("end", function() {
-  			res.send(body);
-  		});
-	}).on('error', function(e) {
-  		//console.log('ERROR: ' + e.message);
-	});
-};
