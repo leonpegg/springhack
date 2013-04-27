4c6fe3cb6a2631974e4bdec1687fd4ec6e63b268
Leon Pegg <leon.pegg@gmail.com>
2013-04-27 15:01:27 +0100

    made map work

--- public/javascripts/app.js
+++ public/javascripts/app.js
@@ -1,18 +1,17 @@
-function init() {
-map = new OpenLayers.Map("map");
-        var mapnik = new OpenLayers.Layer.OSM();
-        map.addLayer(mapnik);
+$(function() {
+	map = new OpenLayers.Map("map");
+	var mapnik = new OpenLayers.Layer.OSM();
+	map.addLayer(mapnik);
 
-        var lonlat = new OpenLayers.LonLat(-1.788, 53.571).transform(
-            new OpenLayers.Projection("EPSG:4326"), // transform from WGS 1984
-            new OpenLayers.Projection("EPSG:900913") // to Spherical Mercator
-          );
+	var lonlat = new OpenLayers.LonLat(-1.788, 53.571).transform(
+		new OpenLayers.Projection("EPSG:4326"), // transform from WGS 1984
+		new OpenLayers.Projection("EPSG:900913") // to Spherical Mercator
+	);
 
-        var zoom = 13;
+	var zoom = 13;
 
-        var markers = new OpenLayers.Layer.Markers( "Markers" );
-        map.addLayer(markers);
-        markers.addMarker(new OpenLayers.Marker(lonlat));
+	var markers = new OpenLayers.Layer.Markers( "Markers" );
+	map.addLayer(markers);
 
-        map.setCenter(lonlat, zoom);
-}
+	map.setCenter(lonlat, zoom);
+});
